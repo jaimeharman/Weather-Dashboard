1b7c1d603d685b5629372e2137fbe0350231219a
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-07-22 22:45:01 -0400

    update functions for get weather and adding to html

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   var appID = "3e684ec40d1ec38b4ab47574d4baeb20";
   getFromLocalStorage();
 
-  //For Loop to create forecast days
+//For Loop to create forecast days
   for (var i = 1; i < 6; i++) {
     var cardMarkup = `
         <div class="card" id="card-day${i}">
@@ -16,17 +16,14 @@ $(document).ready(function () {
     $("#card-deck").append(cardMarkup);
   }
 
-  //user searches for a city's weather
+//user searches for a city's weather
   $("#search-button").on("click", function () {
     var city = $("#city-input").val();
     storeCity(city);
     getCurrentWeather(city);
   });
 
-  function createCityList(citySearchList) {
-    $("#city-list").empty();
-  }
-
+//City input is added to local storage
   var cityArray = [];
   function storeCity(city) {
     cityArray.push(city);
@@ -34,7 +31,7 @@ $(document).ready(function () {
     getFromLocalStorage();
   }
 
-  //refresh the page and saved information stays in place
+//refresh the page and saved information stays in place
   function getFromLocalStorage() {
     if (localStorage.getItem("cities") !== null) {
       //Clear out city list div
@@ -47,6 +44,8 @@ $(document).ready(function () {
     }
   }
 
+
+
   function getCurrentWeather(city) {
     var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appID}`;
     $.ajax({
@@ -54,7 +53,7 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (response) {
       // $("#").text(JSON.stringify(response));
-      console.log(response);
+    addCurrentWeatherInfo(response);
     });
   }
 
@@ -68,6 +67,34 @@ $(document).ready(function () {
       console.log(response);
     });
   }
+
+  function addCurrentWeatherInfo(obj) {
+    console.log(obj);
+    $("#current-wind").append(obj.wind.speed)
+    $("#current-humidity").append(obj.main.humidity)
+    $("#current-temp").append(temperatureConverter(obj.main.temp))
+    $("#current-uv-level").append()
+
+    getCurrentUV(obj.coord.lat, obj.coord.lon);
+
+  }
+
+  function getCurrentUV(lat,lon) {
+    var url = `http://api.openweathermap.org/data/2.5/uvi?appid=${appID}&lat=${lat}&lon=${lon}`
+    $.ajax({
+      url: url,
+      method: "GET",
+    }).then(function (response) {
+      // $("#").text(JSON.stringify(response));
+      console.log(response);
+    });
+  }
+
+//Convert kelvin to fahrenheit 
+  function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+      return ((valNum-273.15)*1.8)+32;
+  }
 });
 
 //cities you've searched are listed(display) and stored in local storage
@@ -76,3 +103,8 @@ $(document).ready(function () {
 //look through object for the weather data
 //append data to DOM
 //5 day forecast
+
+//uv index button/color
+//icons for weather
+//update styling and html
+
